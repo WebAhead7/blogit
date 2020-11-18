6600f0e8bd4758c8de4146a688372bd308db359f
mj majadly <majadly@nevada.unr.edu>
2020-11-18 12:09:18 +0200

    commit

--- database/init.sql
+++ database/init.sql
@@ -1,14 +1,14 @@
 BEGIN;
 
 
-CREATE TABLE users (
+CREATE TABLE ourdata (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   post TEXT NOT NULL,
 );
 
-INSERT INTO users (username, first_name, post) VALUES
+INSERT INTO ourdata (username, first_name, post) VALUES
   ('Sery1976', 'Serya', 'This is my FIRST blog, dsnfso fnldsn lsdng ldn kldskng kdng kdsng kdfngfxkjnkdlng jkdng kdn jfdn jnfdkgn jng dkjng dkjng kd'),
   ('Notne1991', 'Nathan', 'This is my SECOND blog, dsnfso fnldsn lsdng ldn kldskng kdng kdsng kdfngfxkjnkdlng jkdng kdn jfdn jnfdkgn jng dkjng dkjng kd'),
   ('Moull1990', 'Molly', 'This is my THIRD blog, dsnfso fnldsn lsdng ldn kldskng kdng kdsng kdfngfxkjnkdlng jkdng kdn jfdn jnfdkgn jng dkjng dkjng kd'),
--- router.js
+++ router.js
@@ -1,26 +1,27 @@
-const homeHandler = require("./handlers/home");
-const missingHandler = require("./handlers/missing");
-const publicHandler = require("./handlers/public");
-const submitHandler = require("./handlers/submit");
+const homeHandler = require('./handlers/home');
+const missingHandler = require('./handlers/missing');
+const publicHandler = require('./handlers/public');
+const submitHandler = require('./handlers/submit');
 
 function router(request, response) {
-    const url = request.url;
-    const method = request.method;
-    if (url === "/") {
+  const url = request.url;
+  const method = request.method;
+  if (method === 'GET') {
+    if (url === '/') {
       homeHandler(request, response);
+    } else if (url.includes('front-end')) {
+      //loading front-end folder (html css js)
+      publicHandler(request, response);
     }
-    else if (url.includes('front-end')){ //loading front-end folder (html css js)
-      publicHandler(request, response)
-    }
-    else if(url.includes('submit') && method == 'POST') {
+  } else if (method === 'POST') {
+    if (url.includes('submit')) {
       submitHandler(request, response);
     }
     //   else if(url.includes('getdata')) {
     //     getdataHandler(request, response);
-    // } 
-    else {
-      missingHandler(request, response);
-    }
+    // }
+  } else {
+    missingHandler(request, response);
   }
-  
-  module.exports = router;
+}
+module.exports = router;
