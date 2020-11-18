fdf8abf7d74e215d52ed82545863db6373059d33
myassar1211 <myassar1@gmail.com>
2020-11-18 21:47:36 +0200

    update

--- router.js
+++ router.js
@@ -4,7 +4,6 @@ const publicHandler = require('./handlers/public');
 const submitHandler = require('./handlers/submit');
 
 function router(request, response) {
-<<<<<<< HEAD
     const url = request.url;
     const method = request.method;
     if (url === "/") {
@@ -22,19 +21,6 @@ function router(request, response) {
     else {
       missingHandler(request, response);
     }
-=======
-  const url = request.url;
-  const method = request.method;
-  console.log('router: ' + url);
-  if (url === '/') {
-    homeHandler(request, response);
-  } else if (url.includes('public')) {
-    publicHandler(request, response);
-  } else if (url.includes('submit') && method === 'POST') {
-    submitHandler(request, response);
-  } else {
-    missingHandler(request, response);
->>>>>>> f9bfa69bdf62a56e2dd5b11e64b35a4e2b235051
-  }
+  
 }
 module.exports = router;
