4ba629e2e01f7f80db570e45794b32a99e62c4d8
mj majadly <majadly@nevada.unr.edu>
2020-11-19 12:30:47 +0200

    fixingstuff

--- database/init.sql
+++ database/init.sql
@@ -1,19 +1,11 @@
 BEGIN;
-DROP TABLE IF EXISTS ourdata CASCADE;
+DROP TABLE IF EXISTS postpost CASCADE;
 
-CREATE TABLE ourdata (
+CREATE TABLE postpost (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   myname VARCHAR(255) NOT NULL,
   post TEXT NOT NULL
 );
-
-INSERT INTO ourdata (username, myname, post) VALUES
-  ('Sery1976', 'Serya', 'This is my FIRST blog, testttt1'),
-  ('Notne1991', 'Nathan', 'This is my SECOND blog, testttt2'),
-  ('Moull1990', 'Molly', 'This is my THIRD blog, testttt3'),
-  ('Spont1935', 'Spartin', 'This is my FOURTH blog, testttt4'),
-  ('Precand', 'Tracy', 'This is my FIFTH blog, testttt5')
-;
 
 COMMIT;
--- src/models.js
+++ src/models.js
@@ -1,7 +1,7 @@
 const db = require('../database/connection');
 
 function getUserNamePost() {
-  return db.query('SELECT * FROM ourdata').then((results) => {
+  return db.query('SELECT * FROM postpost').then((results) => {
     return results.rows;
   });
 }
@@ -10,7 +10,7 @@ function createNewpost(data) {
   const values = [data.username, data.myname, data.post];
   return db.query(
     `
-      INSERT INTO ourdata(username, myname, post) VALUES($1,$2,$3)`,
+      INSERT INTO postpost(username, myname, post) VALUES($1,$2,$3)`,
     values
   );
 }
